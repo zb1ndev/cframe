21fb0032f2e521126098f2f1bda0f0c14fd0bb12
zb1ndev <zbindev@gmail.com>
2025-04-01 21:50:18 -0500

    Initial Commit

---
+++ src/cframe.h
@@ -0,0 +1,42 @@
+/* cframe - v0.0.1 - MIT License - https://github.com/zb1ndev/zansi.h 
+
+    MIT License
+    Copyright (c) 2025 Joel Zbinden
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+    WARNING : Depends on https://github.com/zb1ndev/zstring.h 
+
+*/
+
+/* Goal for cframe :
+    - Have an easy way to start an HTTP Server and serve content of varying types.
+    - Make routes point to functions that return an HTTP Response.
+    - Have functions that allow for generating HTML and a VDOM.
+        - This will allow for the server to update HTML when client side variables change state.
+    - Simple way to Fetch HTTP Requests in C.
+    
+*/
+
+#if !defined(CFRAME_H)
+#define CFRAME_H
+
+
+
+#endif // CFRAME_H
